c964b2da1d2ebec905d518928bc966d9e795e74c
Matthew Douglas <matt.w.doug@gmail.com>
2019-06-13 11:15:57 -0400

    Set up Pooling System

---
+++ Object Pooling/Assets/Test/TestSpawnee.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawnee : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public string poolName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        Go();
+    }
+
+    private void OnEnable()
+    {
+        Go();
+    }
+
+    private void Go()
+    {
+        rb.velocity = new Vector2(0.0f, 3.0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Test_Collector")
+        {
+            Pool.RemoveObject(this.gameObject, poolName);
+        }
+    }
+}
---
+++ Object Pooling/Assets/Test/TestSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawner : MonoBehaviour
+{
+    public Transform spawnPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            Pool.SpawnObject("spawnee1").transform.position = spawnPos.position;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            Pool.SpawnObject("spawnee2").transform.position = spawnPos.position;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            Pool.SpawnObject("spawnee3").transform.position = spawnPos.position;
+        }
+    }
+}
